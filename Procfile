308ee866057884b1f7e04f0612bb09c1032d6ac6
Michael Dempsey <michaelstdempsey@gmail.com>
2019-10-05 10:38:05 -0400

    Initial Project Check-in

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn seniority_visualizer_app.app:create_app\(\) -b 0.0.0.0:$PORT -w 3
---
+++ autoapp.py
@@ -0,0 +1,5 @@
+# -*- coding: utf-8 -*-
+"""Create an application instance."""
+from seniority_visualizer_app.app import create_app
+
+app = create_app()
