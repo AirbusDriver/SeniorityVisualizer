cebc35aa429283f569b02ca34140c5e38c485202
Michael Dempsey <michaelstdempsey@gmail.com>
2020-02-13 14:24:33 -0500

    Add common base type for shared request objects

--- seniority_visualizer_app/shared/request_object.py
+++ seniority_visualizer_app/shared/request_object.py
@@ -1,13 +1,27 @@
-class InvalidRequestObject(object):
+import typing as t
 
-    def __init__(self):
-        self.errors = []
+class UseCaseRequest:
+
+    @property
+    def errors(self) -> t.List[t.Dict[str, str]]:
+        if not hasattr(self, "_errors"):
+            self._errors: t.List[t.Dict[str, str]] = []
+            return self._errors
+        else:
+            return self._errors
+
+    @errors.setter
+    def errors(self, val):
+        raise AttributeError("errors can not be assigned. Use 'add_error' instead")
 
     def add_error(self, parameter, message):
         self.errors.append({'parameter': parameter, 'message': message})
 
     def has_errors(self):
         return len(self.errors) > 0
+
+
+class InvalidRequestObject(UseCaseRequest):
 
     def __nonzero__(self):
         return False
