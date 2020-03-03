c3128c9ab7c5240a369a2de23c62745abf266dec
Michael Dempsey <michaelstdempsey@gmail.com>
2020-03-02 20:25:34 -0500

    Add retirements pinning to pilot seniority

--- seniority_visualizer_app/seniority/forms.py
+++ seniority_visualizer_app/seniority/forms.py
@@ -1,6 +1,8 @@
 from flask_wtf import FlaskForm
-from wtforms import IntegerField, SubmitField
+from wtforms import IntegerField, SubmitField, BooleanField
 
 
 class BuildPilotPlotForm(FlaskForm):
     employee_id = IntegerField("employee_id")
+    pin_pilot_group = BooleanField(default=True)
+
